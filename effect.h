1c6a9deea55cbadf9ad9b45a5b597644a843303e
marcoalkema <marco.alkema@gmail.com>
2016-03-18 12:16:48 +0100

    Added Effect class files

---
+++ effect.h
@@ -0,0 +1,22 @@
+#ifndef _EFFECT_H_
+#define _EFFECT_H_
+
+#include "audio_io.h"
+
+#define SAMPLERATE		44100
+#define NROFCHANNELS		1
+#define FRAMESPERBUFFER		256
+
+
+class Effect
+{
+public:
+  Effect(); // constructor for base class
+  virtual ~Effect(); // destructor for base class
+  virtual void process_samples(float *inputbuffer,float *outputbuffer);
+private:
+  unsigned long framesperbuffer;
+};
+
+#endif // _EFFECT_H_
+
